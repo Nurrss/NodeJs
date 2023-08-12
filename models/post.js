09b9bb5c95a53395048981c4b54828ee8f70fe3c
Nurs <nurrs.serkul@gmail.com>
2023-08-12 10:38:24 +0600

    lesson17

---
+++ models/post.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  time:{
+    type: Date
+  }
+}, { timestamps: true });
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
---
+++ server.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const Post = require('./models/post');
+const Contact = require('./models/contact');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+const PORT = 3000;
+const db = 'mongodb+srv://nursserkul:nurik_2006@cluster0.fcgbl0o.mongodb.net/node-blog?retryWrites=true&w=majority';
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log('Connected to DB'))
+  .catch((error) => console.log(error));
+
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
+
+app.listen(PORT, (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+app.use(express.static('styles'));
+
+app.use(methodOverride('_method'));
+
+app.get('/', (req, res) => {
+  const title = 'Home';
+  res.render(createPath('index'), { title });
+});
+
+app.get('/contacts', (req, res) => {
+  const title = 'Contacts';
+  Contact
+    .find()
+    .then(contacts => res.render(createPath('contacts'), { contacts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/posts/:id', (req, res) => {
+  const title = 'Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Post
+    .findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit Post';
+  Post
+    .findById(req.params.id)
+    .then(post => res.render(createPath('edit-post'), { post, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  Post
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/posts/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/posts', (req, res) => {
+  const title = 'Posts';
+  Post
+    .find()
+    .sort({ time: -1 })
+    .then(posts => res.render(createPath('posts'), { posts, title }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.get('/add-post', (req, res) => {
+  const title = 'Add Post';
+  res.render(createPath('add-post'), { title });
+});
+
+app.post('/add-post', (req, res) => {
+  const time = new Date()
+  const { title, author, text } = req.body;
+  const post = new Post({ title, author, text, time });
+  post
+    .save()
+    .then((result) => res.redirect('/posts'))
+    .catch((error) => {
+      console.log(error);
+      res.render(createPath('error'), { title: 'Error' });
+    });
+});
+
+app.use((req, res) => {
+  const title = 'Error Page';
+  res
+    .status(404)
+    .render(createPath('error'), { title });
+});
